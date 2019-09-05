b67bb9c7bd9bda1c6a4cc290cadfec318ca832b0
zencloud <mryanmill@gmail.com>
2019-09-05 15:57:10 -0600

    UI Dev Update

--- public/js/app.js
+++ public/js/app.js
@@ -7,23 +7,82 @@
 
 // Modal
 
+// LOAD DATA INTO MODALS
+
+function app_profiles_get() {
+    $.get('/api/profiles', function (data) {
+
+        let myHTML = ``;
+        // Loop through array and load options
+        data.forEach(function (profileData) {
+            console.log(profileData);
+            myHTML += `<option name="${profileData.id}">${profileData.profile_name}</option>`;
+        });
+
+        $('#list-profile-options').html(myHTML);
+    });
+}
+
+
+app_profiles_get();
+
+// Inline Editing
+function app_profile_inline_edit() {
+    // Show Inline Edit
+    $('#inline-edit').hide();
+    $('#inline-save').show();
+}
+
+// Inline Editing
+function app_profile_inline_save() {
+    // Show Inline Edit
+    $('#inline-edit').show();
+    $('#inline-save').hide();
+}
 
 // Create Profile
-function app_profile_create(element, data) {
+function app_profile_create() {
 
-    $.post('/api/profiles/create', packageSent, function (packageGet) {
+    let profileName = $('#input-profile-name').val();
 
+    console.log('Test');
+
+    $.post('/api/profiles/create', profileName, function (packageGet) {
 
         // Failure
         if (!packageGet) {
             // Failure!
-            $(element 'alert-error').show();
+            console.log(packageGet);
         }
 
         // Success
         if (packageGet) {
             // Failure!
-            $(element 'alert-success').show();
+            console.log(packageGet);
+        }
+    });
+}
+
+
+// Create Profile
+function app_profile_create() {
+
+    let profileName = $('#input-profile-name').val();
+
+    console.log('Test');
+
+    $.post('/api/profiles/create', profileName, function (packageGet) {
+
+        // Failure
+        if (!packageGet) {
+            // Failure!
+            console.log(packageGet);
+        }
+
+        // Success
+        if (packageGet) {
+            // Failure!
+            console.log(packageGet);
         }
     });
 }
